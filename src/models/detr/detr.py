5cc713dc0f9082d4ee3a69442ac328b231d5e9c0
Pietrangelo Manco <p.manco@studenti.unipi.it>
2025-02-05 18:34:29 +0100

    Changed the transformer building in the detr model to unify it with the yolos' one.

--- src/models/detr/detr.py
+++ src/models/detr/detr.py
@@ -1,29 +1,35 @@
 import os
-from typing import Dict, Union, List
+from typing import Dict, Union, List, Tuple
 import torch
 from torch import nn, Tensor
 from torch.quantization import quantize_dynamic
 
+from src.models.detr.backbone import tiny, small, base, buildBackbone
 from src.utils.misc import PostProcess
-from .backbone import buildBackbone
-from src.models.transformer import Transformer
 from src.models.mlp import MLP
-
 
 class DETR(nn.Module):
     def __init__(self, args):
         super(DETR, self).__init__()
 
-        self.backbone = buildBackbone(args)
+        # Select the appropriate Transformer
+        if args.detr.transformerName == 'tiny':
+            self.transformer, hidden_dim = tiny()
+        elif args.detr.transformerName == 'small':
+            self.transformer, hidden_dim = small()
+        elif args.detr.transformerName == 'base':
+            self.transformer, hidden_dim = base()
+        else:
+            raise ValueError(f"Backbone '{args.detr.transformerName}' not supported")
 
-        self.reshape = nn.Conv2d(self.backbone.backbone.outChannels, args.hiddenDims, 1)
+        self.backbone = buildBackbone(args, hidden_dim)
+        self.reshape = nn.Conv2d(self.backbone.backbone.outChannels, hidden_dim, 1)
 
-        self.transformer = Transformer(args.hiddenDims, args.numHead, args.numEncoderLayer, args.numDecoderLayer,
-                                       args.dimFeedForward, args.dropout)
+        # Keep these configurable through args
+        self.queryEmbed = nn.Embedding(args.detr.numQuery, hidden_dim)
+        self.class_embed = MLP(hidden_dim, hidden_dim, args.numClass + 1, 3)
+        self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
 
-        self.queryEmbed = nn.Embedding(args.numQuery, args.hiddenDims)
-        self.classEmbed = nn.Linear(args.hiddenDims, args.numClass + 1)
-        self.bboxEmbed = MLP(args.hiddenDims, args.hiddenDims, 4, 3)
 
     def forward(self, x: Tensor, meta=None) -> Dict[str, Union[Tensor, List[Dict[str, Tensor]]]]:
         """
@@ -53,8 +59,8 @@ class DETR(nn.Module):
 
         out = self.transformer(features, mask, query, pos)
 
-        outputsClass = self.classEmbed(out)
-        outputsCoord = self.bboxEmbed(out).sigmoid()
+        outputsClass = self.class_embed(out)
+        outputsCoord = self.bbox_embed(out).sigmoid()
 
         return {'class': outputsClass[-1],
                 'bbox': outputsCoord[-1],
@@ -80,3 +86,26 @@ def buildInferenceModel(args, quantize=False):
     print('optimizing model for inference...')
     return torch.jit.trace(wrapper, (torch.rand(1, 3, args.targetHeight, args.targetWidth).to(device),
                                      torch.as_tensor([args.targetWidth, args.targetHeight]).unsqueeze(0).to(device)))
+
+class DETRWrapper(nn.Module):
+    """ A simple DETR wrapper that allows torch.jit to trace the module since dictionary output is not supported yet """
+
+    def __init__(self, detr, postProcess):
+        super(DETRWrapper, self).__init__()
+
+        self.detr = detr
+        self.postProcess = postProcess
+
+    def forward(self, x: Tensor, imgSize: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+        """
+        :param x: batch images of shape [batchSize, 3, args.targetHeight, args.targetWidth] where batchSize equals to 1
+        If tensor with batchSize larger than 1 is passed in, only the first image prediction will be returned
+
+        :param imgSize: tensor of shape [batchSize, imgWidth, imgHeight]
+
+        :return: the first image prediction in the following order: scores, labels, boxes.
+        """
+
+        out = self.detr(x)
+        out = self.postProcess(out, imgSize)[0]
+        return out['scores'], out['labels'], out['boxes']
