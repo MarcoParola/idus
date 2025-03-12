b2bb506c7c832ed07df73c59c379623f94c2de6a
MarcoParola <marcoparola96@gmail.com>
2025-03-12 22:18:32 +0100

    merge from develop to main

--- test.py
+++ test.py
@@ -1,32 +1,9 @@
 import os
-<<<<<<< HEAD
-import numpy as np
 import torch
-from torch.cuda import amp
-from torch.optim import AdamW
-from torch.optim.lr_scheduler import StepLR
-=======
-import torch
->>>>>>> 9f0f794186df8fdd969e1664236269d695d01cea
 from torch.utils.data import DataLoader
 import wandb
 import hydra
 from tqdm import tqdm
-<<<<<<< HEAD
-
-from src.models import SetCriterion
-from src.datasets import collateFunction, COCODataset
-from src.utils import load_model, load_datasets
-from src.utils.misc import cast2Float
-from src.utils.utils import load_weights
-
-
-@hydra.main(config_path="config", config_name="config")
-def main(args):
-    
-    args.wandbProject = args.wandbProject + '_eval'
-    wandb.init(entity=args.wandbEntity , project=args.wandbProject, config=dict(args))
-=======
 from collections import defaultdict
 
 from src.datasets.dataset import load_datasets, collateFunction
@@ -50,26 +27,10 @@ def accumulate_metrics(metrics_list):
 def main(args):
     args.wandbProject = args.wandbProject + '_eval'
     wandb.init(entity=args.wandbEntity, project=args.wandbProject, config=dict(args))
->>>>>>> 9f0f794186df8fdd969e1664236269d695d01cea
     torch.manual_seed(args.seed)
     device = torch.device(args.device)
     os.makedirs(args.outputDir, exist_ok=True)
 
-<<<<<<< HEAD
-    # load data
-    train_dataset, val_dataset, test_dataset = load_datasets(args)
-    test_dataloader = DataLoader(test_dataset, 
-        batch_size=1, 
-        shuffle=False, 
-        collate_fn=collateFunction,
-        num_workers=args.numWorkers)
-    
-    # load model
-    criterion = SetCriterion(args).to(device)
-    model = load_model(args).to(device)  
-    
-    # multi-GPU training
-=======
     # Load data
     train_dataset, val_dataset, test_dataset = load_datasets(args)
 
@@ -83,33 +44,11 @@ def main(args):
     criterion = SetCriterion(args).to(device)
     model = load_model(args).to(device)
 
->>>>>>> 9f0f794186df8fdd969e1664236269d695d01cea
     if args.multi:
         model = torch.nn.DataParallel(model)
 
     model.eval()
     criterion.eval()
-<<<<<<< HEAD
-    with torch.no_grad():
-        testMetrics = []
-
-        for batch, (imgs, targets) in enumerate(tqdm(test_dataloader)):
-            imgs = imgs.to(device)
-            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-            out = model(imgs)
-            metrics = criterion(out, targets)
-            testMetrics.append(metrics)
-
-        testMetrics = {k: torch.stack([m[k] for m in testMetrics]).mean() for k in testMetrics[0]}
-        for k,v in testMetrics.items():
-            wandb.log({f"test/{k}": v.item()}, step=0)
-    
-    wandb.finish()
-
-   
-if __name__ == '__main__':
-    main()
-=======
 
     with torch.no_grad():
         all_metrics = []
@@ -164,4 +103,3 @@ if __name__ == '__main__':
 
 if __name__ == '__main__':
     main()
->>>>>>> 9f0f794186df8fdd969e1664236269d695d01cea
